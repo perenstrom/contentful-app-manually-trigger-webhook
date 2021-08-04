6a8ef46904fb1196778a547139b9f194efb08cba
Per Enström <enstrom.per@gmail.com>
2021-08-04 20:28:37 +0200

    Clean extra files and add webhook button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
-import { SidebarExtensionSDK } from '@contentful/app-sdk';
+import { Button } from "@contentful/forma-36-react-components";
+import { SidebarExtensionSDK } from "@contentful/app-sdk";
+import { useState } from "react";
 
 interface SidebarProps {
   sdk: SidebarExtensionSDK;
 }
 
+interface InstanceParameters {
+  buttonText: string;
+  webhookUrl: string;
+}
+
 const Sidebar = (props: SidebarProps) => {
-  return <Paragraph>Hello Sidebar Component</Paragraph>;
+  const instanceParameters = props.sdk.parameters
+    .instance as InstanceParameters;
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    const url = instanceParameters.webhookUrl;
+    const options: RequestInit = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    };
+    fetch(url, options)
+      .then((response) => {
+        console.log(response);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Button
+      loading={loading}
+      disabled={loading}
+      isFullWidth={true}
+      icon={"Cycle"}
+      onClick={handleClick}
+    >
+      {instanceParameters.buttonText}
+    </Button>
+  );
 };
 
 export default Sidebar;
